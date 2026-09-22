6f7017cf7ee92d0a83f589faf7c3f6931f526014
Zain-2-84 <zainazhar.qureshi.2.84@gmail.com>
2026-09-22 15:40:02 +0500

    Added a 'Increment Cart Items' button using useState, using old 'Direct state' not 'Updater function'.

--- game-store/src/App.jsx
+++ game-store/src/App.jsx
@@ -4,7 +4,18 @@ import Cart from './components/Cart'
 import GameCard from './components/GameCard'
 import { GamesList } from './components/GamesList'
 
+
 function App() {
+
+  const [cartItems, setCartItems] = useState(0);
+
+  const CartItemsIncrement = () => {
+    // let increment = 0;
+    // increment = increment + 1;
+    // setCartItems(increment);
+
+    return setCartItems(cartItems + 1);
+  }
 
   return (
     <>
@@ -20,6 +31,13 @@ function App() {
         <div className="cart-section">
           <p>Cart section</p>
           <Cart />
+
+          <button onClick={CartItemsIncrement}>
+            Increment Cart Items
+          </button>
+
+          <br /><br />
+          {cartItems}
         </div>
       </div>
     </>
